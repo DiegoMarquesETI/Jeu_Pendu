dd1357657b61444487e9ec53ea056e3e74e783f3
DiegoMarquesETI <75643218+DiegoMarquesETI@users.noreply.github.com>
2020-12-08 08:59:49 +0100

    Add files via upload

--- canvas.py
+++ canvas.py
@@ -5,33 +5,54 @@ Created on Tue Dec  1 10:14:58 2020
 @author: Diego
 """
 from PENDULE import *
+from core import cpt
 
+from tkinter import Tk,Label,Button,Entry,StringVar,Canvas,PhotoImage
 
-from tkinter import Tk,Label,Button,Entry,StringVar
+#Création de la fenêtre de jeu
 Mafenetre=Tk()
 Mafenetre.title('Jeu du pendu')
+
+
+#Création du bouton proposer
 BoutonProposer=Button(Mafenetre,text='Proposer', command=Mafenetre.destroy)
 BoutonProposer.pack(padx=5,pady=5)
 
-
+#Création du bouton entry
 Lettre=StringVar()
 BoutonEntry=Entry(Mafenetre,textvariable=Lettre)
 BoutonEntry.pack(padx=5,pady=50)
 
-x = AffichageMot(MotsFichier(),LUtilise)
+#Label du mot à trouver
+x=Underscore(MotsFichier())
 LabelMotChercher=Label(Mafenetre,text=x,fg='black',bg='white')
 LabelMotChercher.pack(padx=2,pady=10)
 
-cpt=IntVar()
-cpt.set(str(cpt))
-LabelCoup=Label(Mafenetre,textvariable=Coup,fg='black',bg='white')
-LabelCoup.pack(padx=3,pady=20)
+# Label pour indiquer le nombre de coups encore restants
+
+vie=set(str(cpt))
+LabelCoup=Label(Mafenetre,textvariable=vie,fg='black',bg='white')
 
 
-Largeur=600
-Hauteur=450
-Canevas=Canvas(Mafenetre,width=Largeur, height=Hauteur,bg='white')
-Canevas.pack(padx=5,pady=5)
-photo=PhotoImage(file='pendu.gif')
-item=Canevas.create_image(300,200,image=photo)
+
+#Création bouton fermer
+BoutonQuitter=Button(Mafenetre,text='Quitter',command=Mafenetre.destroy)
+
+#Création du canvas
+width=600
+height=450
+Canevas=Canvas(Mafenetre,width=200, height=200,bg='white')
+photo=PhotoImage(file="bonhomme"+str(cpt)+"."+"gif")
+item=Canevas.create_image(100,100,image=photo)
+
+
+
+#Mise en page
+LabelCoup.grid(row=1,sticky='E')
+LabelMotChercher.grid(row=2, sticky='S')
+BoutonProposer.grid(row=3)
+BoutonEntry.grid(row=4)
+BoutonQuitter.grid(row=5, sticky='NW')
+Canevas.grid(row=1,column=2,rowspan=5)
 Mafenetre.mainloop()
+
