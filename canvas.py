d94953505a3aa6b80b44027dc7f8fae0e47e6972
DiegoMarquesETI <75643218+DiegoMarquesETI@users.noreply.github.com>
2020-12-08 10:17:57 +0100

    Add files via upload

--- canvas.py
+++ canvas.py
@@ -13,23 +13,22 @@ from tkinter import Tk,Label,Button,Entry,StringVar,Canvas,PhotoImage
 Mafenetre=Tk()
 Mafenetre.title('Jeu du pendu')
 
-
-#Création du bouton proposer
-BoutonProposer=Button(Mafenetre,text='Proposer', command=Mafenetre.destroy)
-BoutonProposer.pack(padx=5,pady=5)
-
 #Création du bouton entry
 Lettre=StringVar()
 BoutonEntry=Entry(Mafenetre,textvariable=Lettre)
-BoutonEntry.pack(padx=5,pady=50)
+
+
+#Création du bouton proposer
+BoutonProposer=Button(Mafenetre,text='Proposer', command=Saisie)
+
+
 
 #Label du mot à trouver
 x=Underscore(MotsFichier())
 LabelMotChercher=Label(Mafenetre,text=x,fg='black',bg='white')
-LabelMotChercher.pack(padx=2,pady=10)
 
 # Label pour indiquer le nombre de coups encore restants
-
+print(cpt)
 vie=set(str(cpt))
 LabelCoup=Label(Mafenetre,textvariable=vie,fg='black',bg='white')
 
@@ -42,17 +41,18 @@ BoutonQuitter=Button(Mafenetre,text='Quitter',command=Mafenetre.destroy)
 width=600
 height=450
 Canevas=Canvas(Mafenetre,width=200, height=200,bg='white')
-photo=PhotoImage(file="bonhomme"+str(cpt)+"."+"gif")
-item=Canevas.create_image(100,100,image=photo)
+#photo=PhotoImage(file="bonhomme"+str(cpt)+"."+"gif")
+#item=Canevas.create_image(100,100,image=photo)
 
 
 
 #Mise en page
-LabelCoup.grid(row=1,sticky='E')
-LabelMotChercher.grid(row=2, sticky='S')
-BoutonProposer.grid(row=3)
-BoutonEntry.grid(row=4)
-BoutonQuitter.grid(row=5, sticky='NW')
+LabelCoup.grid(row=2)
+LabelMotChercher.grid(row=3)
+BoutonProposer.grid(row=4)
+BoutonEntry.grid(row=5)
+BoutonQuitter.grid(row=6)
 Canevas.grid(row=1,column=2,rowspan=5)
 Mafenetre.mainloop()
+
 
